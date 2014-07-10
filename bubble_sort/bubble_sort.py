5cd353812ed148962af9961bc587f81486a478b4
Lioman <dev@lioman.de>
2014-07-10 18:48:55 +0200

    adding function comment

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,11 +1,13 @@
 __author__ = 'Lioman dev@lioman.de'
 # import list_handling
 
-# bubble sort algorithm
-listi = [4, 5, 6, 2, 1, 89, 88, 3, 5635, 44, 2, 2, 44, 546, 454]
-
 
 def bubble_sort(list):
+    """
+    Sorts a list with the bubble sort algorithm
+    :param list:
+    :return:
+    """
     for i in range(1, len(list) - 1):
         for j in range(len(list) - 1, i-1, -1):
             if list[j - 1] > list[j]:
